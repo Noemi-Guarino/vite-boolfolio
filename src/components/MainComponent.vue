1fd956e04c47b0604166de7dd18ace69e15c2daf
Noemi-Guarino <noemi.guarino.1996@gmail.com>
2024-03-13 17:12:10 +0100

    edit

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,16 +1,20 @@
 <script>
+import axios from 'axios';
 
 export default {
     data() {
         return {
-          message:'main '
+          posts: []
         };
     },
-    // components: {
-    //     AppHeader,
-    //     AppMain,
-    //     AppFooter
-    // },
+    created(){
+      axios.get('http://127.0.0.1:8000/api/posts')
+      .then(res=>{
+          console.log(res.data);
+
+          this.posts = res.data.results.data;
+      })
+    },
     methods: {
 
     }
@@ -19,12 +23,37 @@ export default {
 
 <template>
   <main>
-    {{message}}
+    <h1>
+      Tutti i post
+    </h1>
+
+    <div class="container">
+      <div v-for="post in posts" :key="post.id" class="card">
+        <div>
+          <h2>
+            {{ post.title }}
+          </h2>
+        </div>
+        <div>
+          <ul>
+            <li >
+              Tipologie: {{ post.type.title }}
+            </li>
+            <li v-for="technology in post.technologies" :key="technology.id">
+              Tecnologie: {{ technology.title }}
+            </li>
+          </ul>
+        </div>
+      </div>
+
+    </div>
   </main>
 
 
 </template>
 
 <style scoped>
-
+.card{
+  border: 1px solid black;
+}
 </style>
